20465a13f21a28a5435178e64c57fb70d346c57a
xyshi25 <zherosxy@gmail.com>
2020-06-30 14:26:49 -0400

    update submission code

--- submission/run.sh
+++ submission/run.sh
@@ -7,14 +7,14 @@ mkdir -p Log/Log_KIP
 ./bin/KnapInterdiction
 
 # run experiments for the bilevel vertex cover problem
-mkdir -p Log/Log_BVC_Symm
-mkdir -p Log/Log_BVC_ASymm
-./bin/BVertexCover
+# mkdir -p Log/Log_BVC_Symm
+# mkdir -p Log/Log_BVC_ASymm
+# ./bin/BVertexCover
 
 
-# run experiments for the minimum spanning tree problem
-mkdir -p Log/Log_BMST
-./bin/BMST
+# # run experiments for the minimum spanning tree problem
+# mkdir -p Log/Log_BMST
+# ./bin/BMST
 
 
 # # to clean all the data
